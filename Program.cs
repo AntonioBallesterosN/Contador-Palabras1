411f9ace7062a6470011e2430f987ca9ff1ec2ea
AntonioBallesterosN <106995026+AntonioBallesterosN@users.noreply.github.com>
2022-06-15 11:37:08 -0500

    actualizado

--- Program.cs
+++ Program.cs
@@ -1,10 +1,39 @@
-﻿
-    class Program
-    {
+﻿class Program
+{
 
-        static void Main(string[] args)
+    static void Main(string[] args)
+    {
+        string texto;
+        int espacio;
+        int espacioVocal;
+        int numeroCaracter;
+       
+        Console.Write("Escriba un texto: ");
+        texto = Console.ReadLine();
+
+        espacio = 1;
+        numeroCaracter = 0;
+        espacioVocal = 0;
+
+        while (numeroCaracter < texto.Length)
         {
-        
+            if (texto[numeroCaracter] == ' ')
+            {
+                espacio++;
+            }
+            
+            else if 
+            (texto[numeroCaracter] == ' ' && texto[numeroCaracter] == 'a' || texto[numeroCaracter] == 'e' || texto[numeroCaracter] == 'i' || texto[numeroCaracter] == 'o' || texto[numeroCaracter] == 'u')
+            {
+                espacioVocal++;
+            }
+
+            numeroCaracter++;
         }
+
+        Console.Write("Total de palabras en el texto : {0} {1}", espacio, espacioVocal);
+
+
     }
+}
 
