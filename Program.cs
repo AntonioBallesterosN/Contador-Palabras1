1032aa85f53f3d9ce4b4ce7e738f46655be4d2fd
AntonioBallesterosN <106995026+AntonioBallesterosN@users.noreply.github.com>
2022-06-27 13:53:01 -0500

    Se agrego metodos: Contador de palabras con array Seleccionador de palabras pares

--- Program.cs
+++ Program.cs
@@ -3,13 +3,23 @@
 
     static void Main(string[] args)
     {
-
         Console.Write("Escriba un texto: ");
+       
+
         string textoUsuario = Console.ReadLine();
-        ContadorDePalabras(textoUsuario);
         int numeroDePalabras = ContadorDePalabras(textoUsuario);
-        Console.Write("Total de palabras en el texto : {0}", numeroDePalabras);
+        int numeroDePalabras1= ContadorDePalabrasConArray(textoUsuario);
+
+        Console.WriteLine("");
+        Console.WriteLine("Total de palabras en el texto: {0}", numeroDePalabras);
+        Console.WriteLine("");
+        Console.WriteLine("Total de palabras en el texto con ARRAY: {0}", numeroDePalabras1);
+        Console.WriteLine("");
+        Console.WriteLine("Palabras en el texto que son pares: ");
+
+        SeleccionadorDePalabrasPares(textoUsuario);
     }
+
     static int ContadorDePalabras(string textoUsuario)
     {
         int espacio = 1;
@@ -17,19 +27,37 @@
 
         while (numeroCaracter < textoUsuario.Length)
         {
-
             if (textoUsuario[numeroCaracter] == ' ')
             {
                 espacio++;
             }
             
             numeroCaracter++;
-
         }
         
         return espacio;
-        //hola
     }
-   
-}
 
+    static int ContadorDePalabrasConArray(string textoUsuario)
+    {
+        string[] arreglo = textoUsuario.Split();
+        int numeroDePalabras = arreglo.Length;
+        
+        return numeroDePalabras;    
+    }
+
+    static void SeleccionadorDePalabrasPares(string textoUsuario)
+    {
+        string[] arreglo = textoUsuario.Split(); ;
+        
+        for (int i = 0; i < arreglo.Length; i++)
+        {
+            if ((i % 2) == 1)
+            {
+                string palabras = arreglo[i];
+               
+                Console.WriteLine(palabras);
+            }
+        }
+    }
+}
